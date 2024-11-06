baba5f11b384c5eff875e239149a30a90eccb7e0
duaa <duaaahassan1998@gmail.com>
2024-11-06 17:25:39 +0500

    initial

--- app/core/App.php
+++ app/core/App.php
@@ -7,7 +7,7 @@ class App {
     // Method to split the URL
     private function splitURL() {
         $URL = $_GET['url'] ?? 'home';  // Default to 'home' if 'url' is not set
-        $URL = explode('/', $URL);  // Split URL by '/'
+        $URL = explode('/', trim($URL,'/'));  // Split URL by '/'
         return $URL;
     }
     
@@ -19,16 +19,26 @@ class App {
         // Check if the controller file exists
         if (file_exists($filename)) {
             require $filename;
-            $this->controller= ucfirst($URL[0]); // Include the controller file
-        } else {
+            $this->controller= ucfirst($URL[0]); 
+            unset($URL[0]);
+        }
+         else {
             // If the controller doesn't exist, load the 404 controller
             $filename = "../app/controllers/_404.php";
             require $filename;
             $this->controller = '_404';
         }
-        
-$controller= new $this->controller;
-call_user_func_array([$controller,$this->method], [] );
+        $controller= new $this->controller;
+        if(!empty ($URL[1] ))
+        {
+            if(method_exists($controller,$URL[1])){
+                $this->method=$URL[1];
+                unset($URL[1]);
+            }
+        }
+           show($URL);
+         
+call_user_func_array([$controller,$this->method], $URL );
     }
     
 }
--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,2 +1,1 @@
 <h1> Home Page View</h1>
-<img src="<?=ROOT?>/assets/images/image.jpg">
